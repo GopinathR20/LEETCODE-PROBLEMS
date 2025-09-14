44fb6e65507585136839385048ae8e5c7065257d
Gopinath <161950523+GopinathR20@users.noreply.github.com>
2025-09-14 11:59:15 +0530

    Time: 7 ms (4.95%), Space: 41.9 MB (17.11%) - LeetHub

---
+++ 2299-strong-password-checker-ii/2299-strong-password-checker-ii-09-14-2025-11-59-11.java
@@ -0,0 +1,15 @@
+import java.util.regex.*;
+class Solution {
+    public boolean strongPasswordCheckerII(String password) {
+        String regex="^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&!*()\\-+]).{8,}$";
+        Pattern pattern=Pattern.compile(regex);
+        Matcher matcher=pattern.matcher(password);
+        if(!matcher.matches())return false;
+        for(int i=0;i<password.length()-1;i++){
+            if(password.charAt(i)==password.charAt(i+1)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
